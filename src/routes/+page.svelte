c6dcca3df310654acdbb5643f784fabb89feacb3
MelonEncoder <iangillette@proton.me>
2026-01-04 23:51:54 -0500

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let canvas: HTMLCanvasElement;
+	let ctx;
+
+	onMount(() => {
+		if ((ctx = canvas.getContext('2d'))) {
+			ctx.lineWidth = 4;
+			ctx.strokeStyle = 'solid';
+			ctx.beginPath();
+			ctx.moveTo(20, 20);
+			ctx.lineTo(200, 200);
+			ctx.stroke();
+		}
+	});
+</script>
+
+<main class="main">
+	<canvas class="drawing" bind:this={canvas} width="300px" height="300px"></canvas>
+</main>
+
+<style>
+	.main {
+		display: flex;
+		justify-content: center;
+	}
+	.drawing {
+		border: 1px solid black;
+		align-self: center;
+	}
+</style>
